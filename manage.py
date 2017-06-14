f17440cb97618c22651a2932339e49e0fe1ae00b
dima1422 <plastund@gmail.com>
2017-06-14 22:10:25 +0300

    3

--- manage.py
+++ manage.py
@@ -6,6 +6,7 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pr1.settings")
     a=10
     c=1
+    d=1
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
